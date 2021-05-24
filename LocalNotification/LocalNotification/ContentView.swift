e6d34450ec628380b463584304434b211500d168
Suguru Takahashi <AHA05158@jp.ibm.com>
2021-05-24 11:04:20 +0900

    ローカル通知の設定

--- LocalNotification/LocalNotification/ContentView.swift
+++ LocalNotification/LocalNotification/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    // アプリ起動中でも通知を受け取るためにNotificationControllerをObservedObjectとして設置
+    @ObservedObject var notificationController = NotificationController()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section(header: Text("")) {
+                    NavigationLink(destination: PushSettingView()) {
+                        Text("通知設定")
+                    }
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationBarTitle("設定", displayMode: .inline)
+        }
+        .onAppear {
+            NotificationController.requestAuthorization()
+        }
     }
 }
 
