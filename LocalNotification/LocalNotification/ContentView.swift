6048763f8f65cd15c5aa4fc6d10e7155a39dc023
Suguru Takahashi <AHA05158@jp.ibm.com>
2021-05-24 09:57:25 +0900

    プロジェクトの作成、gitignore作成

---
+++ LocalNotification/LocalNotification/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalNotification
+//
+//  Created by Suguru Takahashi on 2021/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
